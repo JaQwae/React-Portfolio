c0fc72994d3af6cfe32d9b83b1a55aa4dd090b88
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-11-08 22:35:32 -0600

    nav tab functionality working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<App />);
