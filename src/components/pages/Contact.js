9b6fe20beabb43945c1eba51d7be7a7556b699cf
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-11-15 17:36:37 -0600

    temporary format on resume applied

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
-import '../../styles.css/Contact.css'
+import '../../styles.css/Contact.css';
+
 
 
 export default function Contact() {
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,50 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../styles.css/Resume.css";
 
 export default function Resume() {
     return (
-        <div>Resume</div>
+        <div>
+            <h1>Resume</h1>
+            <button class="resume-button">Download My Resume</button>
+            <div className="skill-card-container">
+                <div class="card skills-card" >
+                    <div class="card-header skills-card-header">
+                        Front-end Proficiencies
+                    </div>
+                    <ul class="list-group list-group-flush skills-card-list">
+                        <li class="list-group-item skills-card-list-item">Test 1</li>
+                        <li class="list-group-item skills-card-list-item">Test 2</li>
+                        <li class="list-group-item skills-card-list-item">Test3</li>
+                        <li class="list-group-item skills-card-list-item">Test 4</li>
+                        <li class="list-group-item skills-card-list-item">Test 5</li>
+                    </ul>
+                </div>
+                <div class="card skills-card" >
+                    <div class="card-header skills-card-header">
+                        Back-end Proficiencies
+                    </div>
+                    <ul class="list-group list-group-flush skills-card-list">
+                        <li class="list-group-item skills-card-list-item">Test 1</li>
+                        <li class="list-group-item skills-card-list-item">Test 2</li>
+                        <li class="list-group-item skills-card-list-item">Test3</li>
+                        <li class="list-group-item skills-card-list-item">Test 4</li>
+                        <li class="list-group-item skills-card-list-item">Test 5</li>
+                    </ul>
+                </div>
+                <div class="card skills-card" >
+                    <div class="card-header skills-card-header">
+                        Soft skills
+                    </div>
+                    <ul class="list-group list-group-flush skills-card-list">
+                        <li class="list-group-item skills-card-list-item">Test 1</li>
+                        <li class="list-group-item skills-card-list-item">Test 2</li>
+                        <li class="list-group-item skills-card-list-item">Test3</li>
+                        <li class="list-group-item skills-card-list-item">Test 4</li>
+                        <li class="list-group-item skills-card-list-item">Test 5</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
