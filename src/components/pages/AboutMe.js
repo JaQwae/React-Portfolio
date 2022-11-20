83fb1b554e151ad1b4fb5ba24252ada947c8484c
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-11-19 20:11:32 -0600

    about me page updated and styled

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -9,7 +9,13 @@ export default function AboutMe() {
                     <img  className = "profile-image" src="https://media-exp1.licdn.com/dms/image/C4E03AQGANkwtitDZeA/profile-displayphoto-shrink_800_800/0/1609799385127?e=2147483647&v=beta&t=x43-vFIz_X-ycyzxV9AuijKjjCWHV_IuJIfErDQzsXY" alt="Profile Placeholder"></img>
                 </section>
                 <section className="short-bio">
-                    <p>This is how the text will look. fdkaslfjadflkjasldf asdfj asldfjasd;fa sfhas;f jjasdfna;dsf nasdfjasd;lfj asd;fjasfjasdfh asdifhasdkfh sadlfhlasdkhfsdal bfsdjlfhl asdkjf asdklhf asdjkf fj as;lfdj a;sfj ;asj f;sakdfj ;asj fd;asdjf;kjasdfjk;asf djsadfksadhf sadgjls ldjfbaslj kfh basbhkadfh bESFJHsle</p>
+                    <h3 className="about-me-title-primary">Welcome!</h3>
+                    <p className="about-me-text">Hello, my name is JaQwae Ellison, and I'm a freelance software engineer based in Austin! Currently, I am attending the Web Development Bootcamp at UT Austin, where its core focal point is Javascript! I have designed over two dozen web applications and collaborated with colleges on a few more. I'm eagerly looking to improve my skill set and branch into other technologies and languages.</p>
+                    <h6 className="about-me-title-secondary">My goals:</h6>
+                    <ul className="about-me-list"> 
+                        <li className="about-me-list-items">Make the switch from optometry to tech.</li>
+                        <li className="about-me-list-items">Design inclusive and interactive applications that make people's lives easier</li>
+                    </ul>
                 </section>
             </div>
         </div>
